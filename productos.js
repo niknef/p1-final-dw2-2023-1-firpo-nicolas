2382e9da6366be9030999834a85ef273a8c033c8
NikNef <nicolas.firpo@davinci.edu.ar>
2024-06-19 19:09:27 -0300

    add product list in js

---
+++ productos.js
@@ -0,0 +1,111 @@
+const productos = [
+    { 
+        id: 1,
+        categoria: 'Calzado',
+        nombre: 'Rusty Andreuss Black',
+        descripcion: 'Zapatillas negras de gamuza con goma blanca, posee logo en el extremo lateral posterior. Capellada de canvas. Estilo urbano.',
+        precio: 83390,
+        img: ['img/ANDREUSNEGRA.jpg', 'img/ANDREUSNEGRA1.jpg'],
+        
+    },
+    {
+        id: 2,
+        categoria: 'Calzado',
+        nombre: 'Dc Coourt Graffik White',
+        descripcion: 'Capellada de Sintético + Forrería liviana de lengüeta + Espuma y Cuello con espuma acolchada para máximo confort y soporte Diseño Pill Pattern de DC en pisada. Nueva suela Cupsole con costura.',
+        precio: 109900,
+        img: ['img/GRAFIKWHITEBLACK.jpg', 'img/GRAFIKWHITEBLACK1.jpg', 'img/GRAFIKWHITEBLACK2.jpg', 'img/GRAFIKWHITEBLACK3.jpg'],
+        
+    },
+    {
+        id: 3,
+        categoria: 'Calzado',
+        nombre: 'Dc Manteca High White',
+        descripcion: 'Capellada de descarne vacuno y PU + Refuerzo en puntera y talón + Forerría de Mesh + Plantilla de EVA + Espuma en cuello y lengüeta para mayor soporte y confort + Suela Cupsole de caucho resistente a la abrasión + Pisada con diseño Pill Pattern de DC',
+        precio: 134900,
+        img: ['img/MANTECAHI.jpg', 'img/MANTECAHI1.jpg', 'img/MANTECAHI2.jpg'],
+        
+    },
+    {
+        id: 4,
+        categoria: 'Calzado',
+        nombre: 'Dc Pure Pirate Black',
+        descripcion: 'Capellada de Nobuck sintético, Descarne y/o Sintético + Forrería de Mesh + Cuello y lengüeta acolchados con espuma para mayor amortiguación y protección + Construcción vulcanizada + Suela de caucho resistente a la abrasión + Diseño de pisada “Pill Pattern” de DC.',
+        precio: 109000,
+        img: ['img/purefullblack.jpg', 'img/purefullblack1.jpg', 'img/purefullblack2.jpg'],
+        
+    },
+    {
+        id: 5,
+        categoria: 'Calzado',
+        nombre: 'Rusty Retro Toned',
+        descripcion: 'Zapatillas Blancas de Pu, con detalles en capellada de gamuza de colores pasteles, posee logo en el extremo lateral posterior. Capellada de canvas. Estilo urbano.',
+        precio: 89000,
+        img: ['img/TONED.jpg', 'img/TONED1.jpg'],
+        
+    },
+    {
+        id: 6,
+        categoria: 'Pantalones',
+        nombre: 'Jean Bento Foster',
+        descripcion: 'Jean Bento en color negro gastado, corte regular que se adapta cómodamente a cualquier tipo de cuerpo, este jean es perfecto para cualquier ocasión. El diseño de desgaste le da un toque moderno.',
+        precio: 59600,
+        img: ['img/BENTOFOSTER.jpg', 'img/BENTOFOSTER1.jpg'],
+        
+    },
+    {
+        id: 7,
+        categoria: 'Pantalones',
+        nombre: 'Jean Bento Pantrho',
+        descripcion: 'Jean Bento en color azul, corte regular que se adapta cómodamente a cualquier tipo de cuerpo, este jean es perfecto para cualquier ocasión.',
+        precio: 57400,
+        img: ['img/BENTOPANTRHO.jpg', 'img/BENTOPANTRHO1.jpg'],
+        
+    },
+    {
+        id: 8,
+        categoria: 'Pantalones',
+        nombre: 'Dc Worker Negro',
+        descripcion: 'jean DC en color negro liso y corte slim fit. Diseñado para resaltar tu figura, este jean ofrece un ajuste ceñido que combina comodidad y moda. Su acabado limpio y sin detalles adicionales lo convierte en una opción versátil.',
+        precio: 49900,
+        img: ['img/DCWRKNEGRO.jpg', 'img/DCWRKNEGRO1.jpg'],
+        
+    },
+    {
+        id: 9,
+        categoria: 'Pantalones',
+        nombre: 'Rusty Dungaree',
+        descripcion: 'Jean Rusty en color celeste claro. Este jean, con un corte regular, ofrece un ajuste cómodo que se adapta a cualquier estilo. Lo que lo distingue es el pequeño y elegante estampado en el bolsillo, aportando un detalle único y moderno.',
+        precio: 99900,
+        img: ['img/JeanDungaree.jpeg', 'img/JeanDungaree4.jpeg', 'img/JeanDungaree5.jpeg'],
+        
+    },
+    {
+        id: 10,
+        categoria: 'Buzos',
+        nombre: 'Rusty Biceps Naranja',
+        descripcion: 'Rusty oversize en vibrante color naranja. Este buzo, de corte amplio, ofrece un ajuste relajado que es perfecto para cualquier ocasión, ya sea para una tarde casual o un día activo.',
+        precio: 69900,
+        img: ['img/biceporange.jpeg', 'img/biceporange2.jpeg', 'img/biceporange3.jpeg'],
+        
+    },
+    {
+        id: 11,
+        categoria: 'Buzos',
+        nombre: 'Rusty Grand Prix Blanco',
+        descripcion: 'Su atractivo estampado inspirado en el mundo automotor le añade un toque dinámico y lleno de energía. Perfecto para los entusiastas de los coches y el estilo urbano.',
+        precio: 65900,
+        img: ['img/granprix.jpeg', 'img/granprix1.jpeg','img/granprix2.jpeg'],
+        
+    },
+    {
+        id: 12,
+        categoria: 'Buzos',
+        nombre: 'Rusty Redondo crew',
+        descripcion: 'Añade un toque de elegancia moderna a tu guardarropa con el buzo Rusty Redondo en color negro. Sin capucha y con un diseño de cuello redondo, este buzo ofrece una silueta limpia y clásica.',
+        precio: 49900,
+        img: ['img/redondocrew.jpeg', 'img/redondocrew1.jpeg', 'img/redondocrew2.jpeg', 'img/redondocrew3.jpeg' ],
+        
+    },
+
+];
