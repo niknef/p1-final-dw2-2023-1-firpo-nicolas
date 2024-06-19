5d24f034c2aeb861303f32e9752a1aee1a516b6b
NikNef <nicolas.firpo@davinci.edu.ar>
2024-06-19 19:22:56 -0300

    add abrirCarrito Function to main

--- js/main.js
+++ js/main.js
@@ -247,6 +247,184 @@ function mostrarTotalCarrito() {
     totalCarrito.textContent = `Total: $${total}`;
 };
 
+// Función para abrir el carrito
+function abrirCarrito() {
+    modalCarrito.innerHTML = '';
+
+    // Cambiamos el display none por flex
+    modalCarrito.style.display = 'flex';
+
+    // Creamos el contenedor del carrito
+    const carritoContainer = document.createElement('div');
+    carritoContainer.className = 'carritoContainer';
+
+    // Crear y configurar el header del modal
+    const modalHeader = document.createElement('header');
+    const modalHeaderTitle = document.createElement('h1');
+    modalHeaderTitle.innerText = 'Mi carrito';
+
+    const botonCerrar = document.createElement('button');
+    botonCerrar.className = 'cerrarModal bi bi-x-lg';
+    botonCerrar.addEventListener('click', () => {
+        modalCarrito.style.display = 'none';
+    });
+
+    // Agregamos el título y el botón de cerrar al header
+    modalHeader.append(modalHeaderTitle, botonCerrar);
+
+    // Contenedor de los productos en el carrito
+    const modalProductosContainer = document.createElement('div');
+    modalProductosContainer.className = 'carrito-productos';
+
+    // Agregamos el header y el contenedor de productos al carrito
+    carritoContainer.append(modalHeader, modalProductosContainer);
+
+    // Verificamos si el carrito está vacío
+    if (productosEnCarrito.length === 0) {
+        // Si está vacío, mostramos un mensaje
+        const carritoVacioMensaje = document.createElement('p');
+        carritoVacioMensaje.className = 'carrito-vacio-mensaje';
+        carritoVacioMensaje.innerText = 'Tu carrito está vacío.';
+
+        const carritoVacioIcono = document.createElement('i');
+        carritoVacioIcono.className = 'bi bi-emoji-frown-fill';
+
+        carritoVacioMensaje.append(carritoVacioIcono);
+        modalProductosContainer.appendChild(carritoVacioMensaje);
+    } else {
+        // Si el carrito no está vacío, mostramos los productos
+        productosEnCarrito.forEach(producto => {
+            const modalProducto = document.createElement('div');
+            modalProducto.className = 'carrito-producto';
+
+            // Imagen
+            const imagenProducto = document.createElement('img');
+            imagenProducto.src = producto.img[0];
+            imagenProducto.alt = producto.nombre;
+            imagenProducto.className = 'carrito-producto-imagen';
+
+            // Div con el nombre
+            const contenedorProductoTitulo = document.createElement('div');
+            contenedorProductoTitulo.className = 'carrito-producto-titulo';
+
+            const productoSmall = document.createElement('small');
+            productoSmall.innerText = 'Producto:';
+            
+            const productoTitulo = document.createElement('h3');
+            productoTitulo.innerText = producto.nombre;
+
+            contenedorProductoTitulo.append(productoSmall, productoTitulo);
+
+            // Div con la cantidad
+            const contenedorProductoCantidad = document.createElement('div');
+            contenedorProductoCantidad.className = 'carrito-producto-cantidad';
+
+            const cantidadSmall = document.createElement('small');
+            cantidadSmall.innerText = 'Cantidad:';
+
+            const cantidadProducto = document.createElement('p');
+            cantidadProducto.innerText = producto.cantidad;
+
+            contenedorProductoCantidad.append(cantidadSmall, cantidadProducto);
+
+            // Div con el precio
+            const contenedorProductoPrecio = document.createElement('div');
+            contenedorProductoPrecio.className = 'carrito-producto-precio';
+
+            const precioSmall = document.createElement('small');
+            precioSmall.innerText = 'Precio:';
+
+            const precioProducto = document.createElement('p');
+            precioProducto.innerText = `$${producto.precio}`;
+
+            contenedorProductoPrecio.append(precioSmall, precioProducto);
+
+            // Div con el subtotal
+            const contenedorProductoSubtotal = document.createElement('div');
+            contenedorProductoSubtotal.className = 'carrito-producto-subtotal';
+
+            const subtotalSmall = document.createElement('small');
+            subtotalSmall.innerText = 'Subtotal:';
+            
+            const subtotalProducto = document.createElement('p');
+            subtotalProducto.innerText = `$${producto.precio * producto.cantidad}`;
+
+            contenedorProductoSubtotal.append(subtotalSmall, subtotalProducto);
+
+            // Botón para eliminar producto
+            const eliminarProducto = document.createElement('span');
+            eliminarProducto.className = 'eliminar bi bi-trash-fill';
+
+            eliminarProducto.addEventListener('click', () => {
+                quitarProducto(producto);
+            });
+
+            // Agregamos los elementos al modal
+            modalProducto.append(imagenProducto, contenedorProductoTitulo, contenedorProductoCantidad, contenedorProductoPrecio, contenedorProductoSubtotal, eliminarProducto);
+
+            modalProductosContainer.appendChild(modalProducto);
+        });
+
+        // Creamos la parte inferior del carrito con los botones solo si hay productos
+        const carritoAcciones = document.createElement('div');
+        carritoAcciones.className = 'carrito-acciones';
+
+        const carritoAccionesIzquierda = document.createElement('div');
+        carritoAccionesIzquierda.className = 'carrito-acciones-izquierda';
+
+        const botonVaciar = document.createElement('button');
+        botonVaciar.className = 'carrito-acciones-vaciar';
+        botonVaciar.textContent = 'Vaciar carrito';
+
+        botonVaciar.addEventListener('click', () => {
+            productosEnCarrito = [];
+            cantidadTotal = 0;
+            total = 0;
+
+            actualizarNumerito();
+            mostrarTotalCarrito();
+            abrirCarrito();
+        });
+
+        carritoAccionesIzquierda.appendChild(botonVaciar);
+
+        // TOTAL Y BOTON COMPRAR
+        const carritoAccionesDerecha = document.createElement('div');
+        carritoAccionesDerecha.className = 'carrito-acciones-derecha';
+
+        const carritoAccionesTotal = document.createElement('div');
+        carritoAccionesTotal.className = 'carrito-acciones-total';
+        
+        const totalTextoDerecha = document.createElement('p');
+        totalTextoDerecha.textContent = 'Total:';
+
+        const totalDerecha = document.createElement('p');
+        totalDerecha.textContent = `$${total}`;
+        totalDerecha.setAttribute('id', 'total');
+
+        carritoAccionesTotal.append(totalTextoDerecha, totalDerecha);
+
+        const botonComprar = document.createElement('button');
+        botonComprar.className = 'carrito-acciones-comprar';
+        botonComprar.textContent = 'Comprar';
+
+          // Evento para mostrar el modal de confirmación al hacer clic en "Comprar"
+          botonComprar.addEventListener('click', () => {
+            confirmarCompra();
+        });
+
+        // Agregamos todo al carritoAcciones
+        carritoAccionesDerecha.append(carritoAccionesTotal, botonComprar);
+        carritoAcciones.append(carritoAccionesIzquierda, carritoAccionesDerecha);
+
+        // Añadimos las acciones al contenedor del carrito
+        carritoContainer.appendChild(carritoAcciones);
+    }
+
+    // Finalmente, agregamos el carritoContainer al modal
+    modalCarrito.appendChild(carritoContainer);
+};
+
 // Función para mostrar una notificación
 function mostrarNotificacion() {
     const notificacionCartel = document.createElement('div');
